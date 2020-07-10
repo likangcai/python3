39fcb7fa6bb79207c5ae798c203ba507edcc062c
yxdszlkc <yingzilkq@163.com>
2020-07-10 23:44:26 +0800

    第一次提交

---
+++ Hello_Python/Hello_Python.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author  : 影子
+# @File    : Hello_Python.py
+# @Time    : 2020-07-10 23:39
+# @Software: PyCharm
+#第一次使用github测试
+print("Hello Python3")
---
+++ Hello_Python/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author  : 影子
+# @File    : __init__.py.py
+# @Time    : 2020-07-10 23:37
+# @Software: PyCharm
